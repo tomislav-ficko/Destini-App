b6bb575fad11f89db96e0c2c97e4767e2f3d406d
tomislav-ficko <ficko.tomislav@gmail.com>
2020-08-26 11:18:22 +0200

    save data when app is rotated

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView mStoryTextView;
     private Button mButtonTop;
     private Button mButtonBottom;
+    private int mStoryIndex;
 
 
     @Override
@@ -22,11 +23,29 @@ public class MainActivity extends AppCompatActivity {
         mButtonTop = (Button) findViewById(R.id.buttonTop);
         mButtonBottom = (Button) findViewById(R.id.buttonBottom);
 
+        if (savedInstanceState == null) {
+            mStoryIndex = 1;
+        } else {
+            mStoryIndex = savedInstanceState.getInt("mStoryIndex");
+            mStoryTextView.setText(savedInstanceState.getString("mStoryTextView"));
+            mButtonTop.setText(savedInstanceState.getString("mButtonTop"));
+            mButtonBottom.setText(savedInstanceState.getString("mButtonBottom"));
+        }
     }
 
     public void topButtonClicked(View view) {
     }
 
     public void bottomButtonClicked(View view) {
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt("mStoryIndex", mStoryIndex);
+        outState.putString("mStoryTextView", mStoryTextView.getText().toString());
+        outState.putString("mButtonTop", mButtonTop.getText().toString());
+        outState.putString("mButtonBottom", mButtonBottom.getText().toString());
     }
 }
